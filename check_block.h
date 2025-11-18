8d5485422d8186e7fed69744d55ba804acdfc82e
degetmateo <degetmateo@gmail.com>
2025-11-18 00:49:23 -0300

    removed prints

--- check_block.h
+++ check_block.h
@@ -11,19 +11,19 @@ int check_block (unsigned char data[59], unsigned char prev_hash[32], unsigned c
     unsigned char rehashed[32];
 
     // printf("check_block\n\n");
-    printf("HASH RECREATION\n");
-    printf("HASH RECREATION DATA\n");
-    print_bytes(data, 59);
-    printf("HASH RECREATION PREV_HASH\n");
-    print_bytes(prev_hash, 32);
+    // printf("HASH RECREATION\n");
+    // printf("HASH RECREATION DATA\n");
+    // print_bytes(data, 59);
+    // printf("HASH RECREATION PREV_HASH\n");
+    // print_bytes(prev_hash, 32);
 
-    printf("check_block_actual_hash\n");
-    print_bytes(actual_hash, 32);
+    // printf("check_block_actual_hash\n");
+    // print_bytes(actual_hash, 32);
 
     hash(data, prev_hash, rehashed);
 
-    printf("rehashed_result\n");
-    print_bytes(rehashed, 32);
+    // printf("rehashed_result\n");
+    // print_bytes(rehashed, 32);
 
     return memcmp(rehashed, actual_hash, 32);
 };
